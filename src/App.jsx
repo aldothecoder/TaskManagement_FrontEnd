3e3ade1212318b08171a00d0086688152ff7ab89
aldothecoder <aldodiazfitness@gmail.com>
2024-08-30 19:38:36 -0500

    q

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <Router>
+        <Router basename="/TaskManagement_FrontEnd">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/tasks" element={<Tasks />} />
